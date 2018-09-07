be30e0bf4d36b9e6ee9035206b1a3e6b4e42c389
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2018-09-07 13:44:38 -0700

    Update ASA;DEMAND!APPLICATION!WATER PERMIT!NEW.js
    removed the script to link base record based on BP number/replaced with new script that finds it by address parcel

--- SCRIPTS/EVENT/ASA;DEMAND!APPLICATION!WATER PERMIT!NEW.js
+++ SCRIPTS/EVENT/ASA;DEMAND!APPLICATION!WATER PERMIT!NEW.js
@@ -1,7 +1,3 @@
-// Begin script to associate base premise to water permit
-include("LINK_BASE_PREMISE_TO_RECORD");
-// End script to associate base premise to water permit
-
 // Begin script that copies data from Base Premise record to Water Use Permit record.
 include("COPY_BASE_PREMISE_INFO_TO_WATER_PERMIT");
 // End script that copies data from Base Premise record to Water Use Permit record.
