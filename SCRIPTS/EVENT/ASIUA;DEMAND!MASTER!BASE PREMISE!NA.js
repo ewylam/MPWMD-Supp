1ca9cedb8ff4fd6b78616ed6525870366de85c1e
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2018-09-07 17:02:32 -0700

    Update ASIUA;DEMAND!MASTER!BASE PREMISE!NA.js

--- SCRIPTS/EVENT/ASIUA;DEMAND!MASTER!BASE PREMISE!NA.js
+++ SCRIPTS/EVENT/ASIUA;DEMAND!MASTER!BASE PREMISE!NA.js
@@ -1,7 +1,3 @@
 // Begin script to update the Post Fixture County (ASI) with the sum of all Residential Fixture (ASIT) Post Fixture values.
 include("UPDATE_POST_FIXTURE_UNIT_COUNT");
 // End script to update the Post Fixture County (ASI) with the sum of all Residential Fixture (ASIT) Post Fixture values.
-
-// Begin script to update the Post Fixture County (ASI) with the sum of all Residential Fixture (ASIT) Post Fixture values.
-include("UPDATE_CREDIT_AMOUNT_GRANTED");
-// End script to update the Post Fixture County (ASI) with the sum of all Residential Fixture (ASIT) Post Fixture values.
