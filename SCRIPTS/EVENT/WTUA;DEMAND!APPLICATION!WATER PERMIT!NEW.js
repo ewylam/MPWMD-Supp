5248c6a7751883fec93512341640aa5dc55c1429
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2018-11-29 16:01:23 -0700

    Update WTUA;DEMAND!APPLICATION!WATER PERMIT!NEW.js
    Added new script to assess capacity fee at review = approved

--- SCRIPTS/EVENT/WTUA;DEMAND!APPLICATION!WATER PERMIT!NEW.js
+++ SCRIPTS/EVENT/WTUA;DEMAND!APPLICATION!WATER PERMIT!NEW.js
@@ -1,3 +1,6 @@
 // Begin script to update the parent Base Premise record with info from Water Permit
 include("UPDATE_BASE_PREMISE_FROM_WATER_PERMIT");
 // End script to update the parent Base Premise record with info from Water Permit
+// Begin script to assess capacity fee at approved - fee formula uses fee indicator on field Adjusted Water Use Capacity
+include("ASSESS_CAPACITY_FEES");
+// End script to assess capacity fee
