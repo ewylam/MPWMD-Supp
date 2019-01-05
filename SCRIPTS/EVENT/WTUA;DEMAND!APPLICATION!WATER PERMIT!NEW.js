d49c2b79905b75e1f787470831b8e9b55d6380a1
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2019-01-04 17:16:53 -0700

    Update WTUA;DEMAND!APPLICATION!WATER PERMIT!NEW.js
    added the 2 new scripts for deeds table updates and assess fees for deed recordings and capacity fee  Byrne/Tony l 12/28/2018

--- SCRIPTS/EVENT/WTUA;DEMAND!APPLICATION!WATER PERMIT!NEW.js
+++ SCRIPTS/EVENT/WTUA;DEMAND!APPLICATION!WATER PERMIT!NEW.js
@@ -1,6 +1,9 @@
 // Begin script to update the parent Base Premise record with info from Water Permit
 include("UPDATE_BASE_PREMISE_FROM_WATER_PERMIT");
 // End script to update the parent Base Premise record with info from Water Permit
-// Begin script to assess capacity fee at approved - fee formula uses fee indicator on field Adjusted Water Use Capacity
-include("ASSESS_CAPACITY_FEES");
-// End script to assess capacity fee
+// Begin script to add rows to custom list (Data Table) DEED RESTRICTIONS 
+include("ADD_ROWS_TO_DEED_RESTRICTIONS");
+// End script to add rows to custom list DEED RESTRICTIONS
+// Being script to add recording and capacity fees
+include("ADD_RECORDING_AND_CAPACITY_FEES");
+// End script to add recording and capacity fees
