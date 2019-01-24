5e098f10e445aaa3372d341fc095d7125796780c
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2019-01-24 15:42:32 -0700

    Delete ASSESS_CAPACITY_FEES.js
    removing - consolidated with recording and assessing capacity fee

--- SCRIPTS/MPWMD EVENT SCRIPTS/ASSESS_CAPACITY_FEES.js
+++
@@ -1,3 +0,0 @@
-//When workflow task Review is set to status Approved, and calculate capacity fee is checked assess fee PM_CAPACITY, Qty 1
-  if(wfTask == "Review" && wfStatus == "Approved" &&  {
-    
