e125b0fe3dd916e90a9c0bdce6371f470898d4af
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2019-01-25 13:50:06 -0700

    Update UPDATE_PROPOSED_WATER_USAGE.js
    updated Current Permit Onsite Credit field when proposed water usage <0

--- SCRIPTS/MPWMD EVENT SCRIPTS/UPDATE_PROPOSED_WATER_USAGE.js
+++ SCRIPTS/MPWMD EVENT SCRIPTS/UPDATE_PROPOSED_WATER_USAGE.js
@@ -3,6 +3,7 @@ var vPostFixtureUnitCount;
 var v2ndBathFixtureCount;
 var vCurrentFixtureUnitCount;
 var vProposedWaterUsage;
+var vCurrentPermitOnsiteCredit;
 vPostFixtureUnitCount = getAppSpecific("Post Fixture Unit Count");
 v2ndBathFixtureCount = getAppSpecific("Post 2nd Bath Fixture");
 vCurrentFixtureUnitCount = getAppSpecific("Current Fixture Unit Count");
@@ -22,5 +23,10 @@ if (vCurrentFixtureUnitCount != null && vCurrentFixtureUnitCount != "") {
 vProposedWaterUsage = (vPostFixtureUnitCount - vCurrentFixtureUnitCount) * .01; // Convert to AF
 if (parseFloat(vProposedWaterUsage) != "NaN") {
 	editAppSpecific("Proposed Water Usage", toFixed(vProposedWaterUsage, 4));
+}
+//if less water demand (Proposed water usage <0)put the absolute value of the proposed water usage into the Current Permit Onsite Credit
+if (parseFloat(vProposedWaterUsage) < 0) {
+vCurrentPermitOnsiteCredit = Math.abs(toFixed(vCurrentPermitOnsiteCredit, 4));
+                editAppSpecific("Current Permit Onsite Credit", toFixed(vCurrentPermitOnsiteCredit, 4));
 }
 // End script to update the Proposed Water Usage (ASI) with the result of the Post Fixture Unit Count and Post 2nd Bath Fixture (ASI) minus Current Fixture Unit Count (ASI)
