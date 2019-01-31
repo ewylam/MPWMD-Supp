20292579b40e0ff63763fe36369b058148a4aa48
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2019-01-31 07:29:40 -0700

    Update ASSESS_WATER_PERMIT_FEES.js
    changed fees to auto invoice

--- SCRIPTS/MPWMD EVENT SCRIPTS/ASSESS_WATER_PERMIT_FEES.js
+++ SCRIPTS/MPWMD EVENT SCRIPTS/ASSESS_WATER_PERMIT_FEES.js
@@ -1,9 +1,9 @@
 // Begin script to assess water permit fees
 var vPermitCategory = getAppSpecific("Permit Category");
 if (vPermitCategory == "Residential") {
-	updateFee("PM_APPRES","MP_PERMIT","Final", 1, "N");
+	updateFee("PM_APPRES","MP_PERMIT","Final", 1, "Y");
 }
 else if (vPermitCategory == "Non-Residential" || vPermitCategory == "Mixed=Use") {
-	updateFee("PM_APPNONRES","MP_PERMIT","Final", 1, "N");
+	updateFee("PM_APPNONRES","MP_PERMIT","Final", 1, "Y");
 }
 // End script to assess water permit fees
