1309442e565739c4749ff73acc73af228f1ee03a
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2019-02-20 16:04:23 -0700

    Update ASA;DEMAND!APPLICATION!CONSERVATION!~.js
    Added copy from base premise script

--- SCRIPTS/EVENT/ASA;DEMAND!APPLICATION!CONSERVATION!~.js
+++ SCRIPTS/EVENT/ASA;DEMAND!APPLICATION!CONSERVATION!~.js
@@ -1,3 +1,6 @@
 // Begin script to find Base Premise records by parcel and relate as parent
 include("FIND_RELATE_BASE_PREMISE");
 // End script to find Base Premise records by parcel
+// Begin script that copies data from Base Premise record to Water Use Permit record.
+include("COPY_BASE_PREMISE_INFO_TO_WATER_PERMIT");
+// End script that copies data from Base Premise record to Water Use Permit record.
