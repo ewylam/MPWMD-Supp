2dde9c4299b624dd99daa0345571dce5a5026efd
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2019-04-10 15:13:10 -0700

    Update ASSESS_WATER_PERMIT_FEES.js
    fixed Mixed-Use

--- SCRIPTS/MPWMD EVENT SCRIPTS/ASSESS_WATER_PERMIT_FEES.js
+++ SCRIPTS/MPWMD EVENT SCRIPTS/ASSESS_WATER_PERMIT_FEES.js
@@ -3,7 +3,7 @@ var vPermitCategory = getAppSpecific("Permit Category");
 if (vPermitCategory == "Residential") {
 	updateFee("PM_APPRES","MP_PERMIT","FINAL", 1, "Y");
 }
-else if (vPermitCategory == "Non-Residential" || vPermitCategory == "Mixed=Use") {
+else if (vPermitCategory == "Non-Residential" || vPermitCategory == "Mixed-Use") {
 	updateFee("PM_APPNONRES","MP_PERMIT","FINAL", 1, "Y");
 }
 // End script to assess water permit fees
