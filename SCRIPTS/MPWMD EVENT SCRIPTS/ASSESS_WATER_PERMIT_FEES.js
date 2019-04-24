b4d6d01c11d6baac9c4cf056abb138b3e877d889
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2019-04-24 14:11:10 -0700

    Update ASSESS_WATER_PERMIT_FEES.js
    moving script from WTUA to ASA - removed if wtua logic

--- SCRIPTS/MPWMD EVENT SCRIPTS/ASSESS_WATER_PERMIT_FEES.js
+++ SCRIPTS/MPWMD EVENT SCRIPTS/ASSESS_WATER_PERMIT_FEES.js
@@ -1,11 +1,10 @@
 // Begin script to assess water permit fees
-if(wfTask == "Application Received" && wfStatus == "Accepted"){
-	var vPermitCategory = getAppSpecific("Permit Category");
-		if (vPermitCategory == "Residential") {
-		updateFee("PM_APPRES","MP_PERMIT","FINAL", 1, "Y");
+var vPermitCategory = getAppSpecific("Permit Category");
+	if (vPermitCategory == "Residential") {
+	updateFee("PM_APPRES","MP_PERMIT","FINAL", 1, "Y");
 	}
 	else if (vPermitCategory == "Non-Residential" || vPermitCategory == "Mixed-Use") {
 		updateFee("PM_APPNONRES","MP_PERMIT","FINAL", 1, "Y");
-}
-}
+	}
+
 // End script to assess water permit fees
