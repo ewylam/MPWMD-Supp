26f016ed2208f724994164b52844697e9513a2e5
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2019-05-03 12:38:30 -0700

    Update UPDATE_CURRENT_FIXTURE_UNIT_COUNT_COMMERCIAL.js
    3 decimals

--- SCRIPTS/MPWMD EVENT SCRIPTS/UPDATE_CURRENT_FIXTURE_UNIT_COUNT_COMMERCIAL.js
+++ SCRIPTS/MPWMD EVENT SCRIPTS/UPDATE_CURRENT_FIXTURE_UNIT_COUNT_COMMERCIAL.js
@@ -13,6 +13,6 @@ if (typeof(COMMERCIAL) == "object") {
 	}
 }
 if (vTotalFixtureCount != "NaN") {
-	editAppSpecific("Non-Residential AF Total", toFixed(vTotalFixtureCount, 2));
+	editAppSpecific("Non-Residential AF Total", toFixed(vTotalFixtureCount, 3));
 }
 // End script to update the Non-Residential Current Fixture Count with the sum of all Commercial (ASIT) Fixture values.
