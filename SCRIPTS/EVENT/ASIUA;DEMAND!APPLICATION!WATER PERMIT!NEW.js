ea583ba704c110302dac1f6a324af27ae0048566
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2019-05-15 16:41:59 -0700

    Update ASIUA;DEMAND!APPLICATION!WATER PERMIT!NEW.js
    re order execution

--- SCRIPTS/EVENT/ASIUA;DEMAND!APPLICATION!WATER PERMIT!NEW.js
+++ SCRIPTS/EVENT/ASIUA;DEMAND!APPLICATION!WATER PERMIT!NEW.js
@@ -2,10 +2,6 @@
 include("UPDATE_POST_FIXTURE_UNIT_COUNT");
 // End script to update the Post Fixture County (ASI) with the sum of all Residential Fixture (ASIT) Post Fixture values.
 
-// Begin script to update the Proposed Water Usage (ASI) with the result of the Post Fixture Unit Count (ASI) minus Current Fixture Unit Count (ASI)
-include("UPDATE_PROPOSED_WATER_USAGE");
-// End script to update the Proposed Water Usage (ASI) with the result of the Post Fixture Unit Count (ASI) minus Current Fixture Unit Count (ASI)
-
 // Begin script to update the Non-Residential Current Fixture Count with the sum of all Commercial (ASIT) Fixture values.
 include("UPDATE_CURRENT_FIXTURE_UNIT_COUNT_COMMERCIAL");
 // End script to update the Non-Residential Current Fixture Count with the sum of all Commercial (ASIT) Fixture values.
@@ -13,6 +9,10 @@ include("UPDATE_CURRENT_FIXTURE_UNIT_COUNT_COMMERCIAL");
 // Begin script to update the Post Non-Residential with the sum of all Commercial (ASIT) Post Capacity values.
 include("UPDATE_POST_CAPACITY_COUNT_COMMERCIAL");
 // End script to update the Post Non-Residential with the sum of all Commercial (ASIT) Post Capacity values.
+
+// Begin script to update the Proposed Water Usage (ASI) with the result of the Post Fixture Unit Count (ASI) minus Current Fixture Unit Count (ASI)
+include("UPDATE_PROPOSED_WATER_USAGE");
+// End script to update the Proposed Water Usage (ASI) with the result of the Post Fixture Unit Count (ASI) minus Current Fixture Unit Count (ASI)
 
 // Begin script to convert old fixture names to new ones and add the sort order
 include("UPDATE_FIXTURE_TYPE_AND_SORT_ORDER");
