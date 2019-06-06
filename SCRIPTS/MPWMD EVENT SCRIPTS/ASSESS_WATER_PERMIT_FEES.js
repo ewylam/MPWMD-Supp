f8eed914a2c852844cc67bcd926274a20d6a2a68
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2019-06-06 13:03:00 -0700

    Update ASSESS_WATER_PERMIT_FEES.js
    changed to include Amendment Fee if = Yes

--- SCRIPTS/MPWMD EVENT SCRIPTS/ASSESS_WATER_PERMIT_FEES.js
+++ SCRIPTS/MPWMD EVENT SCRIPTS/ASSESS_WATER_PERMIT_FEES.js
@@ -1,7 +1,9 @@
 // Begin script to assess water permit fees
 var vPermitCategory = getAppSpecific("Permit Category");
 var vPermitType = getAppSpecific("Permit Type");
-if (vPermitType != "Landscape") {
+var vAmendment = getAppSpecific("Amendment");
+logDebug("Amendment :" + vAmendment);
+if (vPermitType != "Landscape" && vAmendment != "Yes") {
 	if (vPermitCategory == "Residential") {
 		updateFee("PM_APPRES", "MP_PERMIT", "FINAL", 1, "Y");
 	} else if (vPermitCategory == "Non-Residential" || vPermitCategory == "Mixed-Use") {
@@ -9,5 +11,12 @@ if (vPermitType != "Landscape") {
 	}
 } else if (vPermitType == "Landscape") {
 	updateFee("PM_LANDS_REV","MP_PERMIT","FINAL", 1, "Y");
+}
+if(vAmendment == "Yes" && vPermitType != "Landscape"){
+	if (vPermitCategory == "Residential") {
+		updateFee("PM_AMRES", "MP_PERMIT", "FINAL", 1, "N");
+	} else if (vPermitCategory == "Non-Residential" || vPermitCategory == "Mixed-Use") {
+		updateFee("PM_AMNONRES", "MP_PERMIT", "FINAL", 1, "N");
+	}
 }
 // End script to assess water permit fees
