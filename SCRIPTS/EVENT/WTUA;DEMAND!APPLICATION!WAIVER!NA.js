85cda5931a8440424609a6fe36554f632c0bff5a
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2019-06-07 15:22:19 -0700

    Update WTUA;DEMAND!APPLICATION!WAIVER!NA.js
    Update expiration date from Workflow

--- SCRIPTS/EVENT/WTUA;DEMAND!APPLICATION!WAIVER!NA.js
+++ SCRIPTS/EVENT/WTUA;DEMAND!APPLICATION!WAIVER!NA.js
@@ -1,7 +1,9 @@
 // Begin script to create child meter permits for each row in the meter table
-include("CREATE_METER_PERMITS");
+//include("CREATE_METER_PERMITS");
 // End script to create child meter permits for each row in the meter table
-
+// Begin script to update Meter Permit Expiration date to 60 days
+include("UPDATE_EXPIRATION_DATE_ASI");
+// End script to update Meter Permit Expiration date to 60 days
 // Begin script to close workflow and related records workflow.
 include("CLOSE_METER_PERMITS");
 // End script to close workflow and related records workflow.
