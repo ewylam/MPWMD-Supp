e7c4a8490854efb9ddc7d56d4fa62aadc9f2bf7e
lindacharron13 <39282304+lindacharron13@users.noreply.github.com>
2019-08-20 14:33:39 -0700

    Create UPDATE_EXPIRATION_METER_PERMITS.js

---
+++ SCRIPTS/MPWMD EVENT SCRIPTS/UPDATE_EXPIRATION_METER_PERMITS.js
@@ -0,0 +1,28 @@
+// Begin script to update the expiration date for Meter Permits 1 year
+if (matches(wfTask,"Permit Issuance", "Inspection") && matches(wfStatus,"Issued", "Issued in Zone", "Amendment Issued")) {
+	var vToday;
+	var vToday_mm;
+	var vToday_dd;
+	var vToday_yyyy;
+	var vExpDateString;
+	var vIssuedDateString;
+	
+	//get today as a string "MM/DD/YYYY"
+	vToday = new Date(wfDateMMDDYYYY);
+	vToday_mm = vToday.getMonth() + 1;
+	vToday_mm = (vToday_mm < 10) ? '0' + vToday_mm : vToday_mm;
+	vToday_dd = vToday.getDate();
+	vToday_dd = (vToday_dd < 10) ? '0' + vToday_dd : vToday_dd;
+	vToday_yyyy = vToday.getFullYear();
+	vExpDateString = vToday_mm + "/" + vToday_dd + "/" + (vToday_yyyy + 1);
+	vIssuedDateString = wfDateMMDDYYYY;
+	
+	//Save to ASI Expiration Date
+	if (vExpDateString != null && vExpDateString!= "") {
+		editAppSpecific("Expiration Date", vExpDateString);
+	}
+	if (vIssuedDateString != null && vIssuedDateString!= "") {
+		editAppSpecific("Issued Date", vIssuedDateString);
+	}
+}
+// End script to update the expiration date
